e985afecf8c153853929b197a0b49867e83dcded
silvestrevivo <silvestrevivomillan@gmail.com>
2018-11-17 20:36:45 +0100

    creating conditional from actions in navigation to reuse layout

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
 import { CardSection } from './common'
+import { Actions } from 'react-native-router-flux';
 
 class ListItem extends Component {
+
+  onRowPress = () => {
+    Actions.employeeCreate({ employee: this.props.employee })
+    // we are giving an aditional props to this layout
+    // on this way we can use a conditional on EmployeeCreate component
+    // to decide if we want to use the layout to create or to edit a user
+  }
+
   render() {
     const { name } = this.props.employee.item;
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>{name}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={this.onRowPress}>
+        <View>
+          <CardSection>
+            <Text style={styles.titleStyle}>{name}</Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
