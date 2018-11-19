401b067151f08c9ef65a579be010b090c770dc82
silvestrevivo <silvestrevivomillan@gmail.com>
2018-11-19 08:56:45 +0100

    logout button added with actions reset

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, View, StyleSheet } from 'react-native'
+import { Button } from './common'
 import { connect } from 'react-redux'
 import { employeesFetch } from '../actions';
 import ListItem from './ListItem'
+import firebase from 'firebase';
 import _ from 'lodash'
+import { Actions } from 'react-native-router-flux';
 
 class EmployeeList extends Component {
   componentDidMount() {
@@ -16,16 +19,39 @@ class EmployeeList extends Component {
     return <ListItem employee={employee} />;
   }
 
+  onPressButton = () => {
+    firebase.auth().signOut();
+    Actions.auth({ type: 'reset' })
+  }
+
   render() {
     return (
-      <FlatList
-        data={this.props.employees}
-        renderItem={this.renderRow}
-        keyExtractor={employee => employee.uid}
-      />
+      <View style={styles.appContainer}>
+        <FlatList
+          data={this.props.employees}
+          renderItem={this.renderRow}
+          keyExtractor={employee => employee.uid}
+        />
+        <View style={styles.buttonContainerStyle}>
+          <Button onPress={this.onPressButton}>Log Out</Button>
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1
+  },
+  buttonContainerStyle: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 20
+  }
+})
+
 
 const mapStateToProps = state => {
   const employees = _.map(state.employees, (val, uid) => {
